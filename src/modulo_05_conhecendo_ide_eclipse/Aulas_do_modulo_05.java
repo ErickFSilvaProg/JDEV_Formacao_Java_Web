3a90370253e8386b58d4489b44f41edce1b6018f
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-07-24 15:15:51 -0300

    JDEV_Formacao_Java_Web, 24/07/2025 (tarde)

---
+++ src/modulo_05_conhecendo_ide_eclipse/Aulas_do_modulo_05.java
@@ -0,0 +1,14 @@
+package modulo_05_conhecendo_ide_eclipse;
+
+public class Aulas_do_modulo_05 {
+	
+	// 'main' é um método auto executável em java.
+	public static void main(String[] args) {
+		
+		// Escreve texto no console.
+		System.out.println("Meu primeiro programa em Java");
+		System.out.println("Curso Formação Java Web");
+		
+	}
+
+}
---
+++ src/modulo_06_github_versionamento_codigo/Aulas_do_modulo_06.java
@@ -0,0 +1,5 @@
+package modulo_06_github_versionamento_codigo;
+
+public class Aulas_do_modulo_06 {
+
+}
